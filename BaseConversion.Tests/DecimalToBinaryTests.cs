8d052316642fb22725bec189bd027bbe40c6913d
Dan Tarjeft <dtarjeft@gmail.com>
2015-02-26 13:09:36 -0500

    Lab 03 complete- System now converts between base 2 through 10. Will require refactoring to support bases beyond that.

---
+++ BaseConversion.Tests/DecimalToBinaryTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BaseConversion.Tests
+{
+    using NUnit.Framework;
+    [TestFixture]
+    public class DecimalToBinaryTests
+    {
+        [Test]
+        [TestCase(34, 100010)]
+        [TestCase(6, 110)]
+        [TestCase(5,101)]
+        [TestCase(3,11)]
+        public void BaseConversion_10to2(int arg1, int arg2)
+        {
+            Evaluate(arg1, arg2, 10, 2);
+        }
+
+        [Test]
+        [TestCase(100010, 34)]
+        [TestCase(110, 6)]
+        [TestCase(101, 5)]
+        [TestCase(11, 3)]
+        public void BaseConversion_2to10(int arg1, int arg2)
+        {
+            Evaluate(arg1, arg2, 2, 10);
+        }
+
+        [Test]
+        [TestCase(new[] {11, 1110}, 10001)]
+        public void AddBinaryReturnBinary(int[] argInts, int result)
+        {
+            var output = new NumberWithBase(0,2);
+            output = argInts.Aggregate(output, (current, argInt) => current + new NumberWithBase(argInt, 2));
+            Assert.AreEqual(result, output.Value);
+        }
+
+        [Test]
+        [TestCase(new[] { 1010,111 }, 17)]
+        public void AddBinaryReturnDecimal(int[] argInts, int result)
+        {
+            var output = new NumberWithBase(0, 2);
+            output = argInts.Aggregate(output, (current, argInt) => current + new NumberWithBase(argInt, 2));
+            Console.Write(output.Value);
+            output.ChangeBase(10);
+            Assert.AreEqual(result, output.Value);
+        }
+
+        private static void Evaluate(int input, int expected, int sourceBase, int targetBase)
+        {
+            var test = new NumberWithBase(input, sourceBase);
+            test.ChangeBase(targetBase);
+            Assert.AreEqual(expected, test.Value);
+        }
+    }
+}
---
+++ BinaryDecimalConversion/NumberWithBase.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace BaseConversion
+{
+    public class NumberWithBase
+    {
+        
+        public int Value { get; private set; }
+        public int BaseValue { get; private set; }
+
+        public NumberWithBase(int value, int baseValue = 10)
+        {
+            BaseValue = baseValue;
+            Value = value;
+        }
+
+        public void ChangeBase(int newBase)
+        {
+            var oldVal = Value;
+            Value = 0;
+            var evenFlag = oldVal%2 == 0;
+
+            while (oldVal != 0)
+            {
+                Value *= BaseValue;
+                Value += oldVal%newBase;
+                oldVal /= newBase;
+            }
+            if (evenFlag)
+            {
+                Value *= BaseValue;
+            }
+            BaseValue = newBase;
+        }
+
+        private void _checkBase()
+        {
+            var oldVal = Value;
+            var newVal = 0;
+            var evenFlag = oldVal %2 == 0;
+
+            while (oldVal != 0)
+            {
+                var token = oldVal%10;
+                if (token >= BaseValue)
+                {
+                    oldVal -= BaseValue;
+                    oldVal += 10;
+                    continue;
+                }
+                oldVal /= 10;
+                newVal *= 10;
+                newVal += token;
+            }
+            if (evenFlag)
+            {
+                newVal *= 10;
+            }
+            Value = newVal;
+
+        }
+
+
+        public static NumberWithBase operator +(NumberWithBase n1, NumberWithBase n2)
+        {
+            if (n2.BaseValue != n1.BaseValue)
+            {
+                n2.ChangeBase(n1.BaseValue);
+            }
+            var numToAdd = new NumberWithBase( n1.Value + n2.Value, n1.BaseValue);
+            numToAdd._checkBase();
+            return numToAdd;
+        }
+    }
+}
